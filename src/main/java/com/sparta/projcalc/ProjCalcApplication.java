5bc91b492170ad7f9e4108b14271dcde469938d5
kwonkiyong0059 <kkoung425@gmail.com>
2024-08-30 19:31:49 +0900

    update : 프로젝트 설정 파일 업데이트

--- src/main/java/com/sparta/projcalc/ProjCalcApplication.java
+++ src/main/java/com/sparta/projcalc/ProjCalcApplication.java
@@ -2,12 +2,18 @@ package com.sparta.projcalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class ProjCalcApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProjCalcApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
